b48bf855ce857fb2e0e2d7c04dbd053218dab091
clemjouve <clementanantes@gmail.com>
2026-07-29 16:15:21 +0200

    fix: click-blocking removed

--- public/src/utils/body-content.js
+++ public/src/utils/body-content.js
@@ -1,9 +1,3 @@
- document.captureEvents(Event.MOUSEDOWN | Event.MOUSEUP | Event.CLICK);
-        document.onmousedown = clickIE4;
-        document.onmouseup = clickIE4;
-        document.onclick = clickIE4;
-
-        function clickIE4()
-        {
-            return false; 
-        }
+(function () {
+    // Global click-blocking removed to allow links and buttons to work.
+})();
